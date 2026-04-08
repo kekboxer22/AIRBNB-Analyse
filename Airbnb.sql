fa3fb6517bed03bb0bd52ab17fa8a1c69032d6a0
kekboxer22 <nauma8789@gmail.com>
2026-04-08 22:45:17 +0200

    Delete Airbnb.sql

--- Airbnb.sql
+++
@@ -1,2 +0,0 @@
-SELECT id, "name", host_id, host_name, neighbourhood, latitude, longitude, room_type, price, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365, number_of_reviews_ltm, license, city, scrape_date
-FROM listings;
